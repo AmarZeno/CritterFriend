b9110ff9eeb447286d3549604c30665bed744d9d
Ajay Sathish <u1068959@ad.utah.edu>
2016-11-12 00:32:52 -0700

    Added siren indicator

---
+++ CritterFriend/Assets/Scripts/WorldUpdateHandler.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WorldUpdateHandler : MonoBehaviour {
+
+    public GameObject SpawnPoacher;
+    public GameObject SpawnedPoacher;
+    public GameObject IndicatorPrefab;
+    public GameObject Indicator;
+
+    // Use this for initialization
+    void Start () {
+        SpawnPoacherMethod();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float MoveForward = Input.GetAxis("Vertical");
+        SpawnedPoacher.transform.Translate(Vector3.right * Time.deltaTime);
+    }
+
+    void SpawnPoacherMethod()
+    {
+        Vector3 newPos = new Vector3(-15f, 0.5f, 0);
+        SpawnedPoacher = Instantiate(SpawnPoacher, newPos, Quaternion.identity) as GameObject;
+        Indicator = Instantiate(IndicatorPrefab) as GameObject;
+        Indicator.transform.parent = SpawnedPoacher.transform;
+    }
+}
