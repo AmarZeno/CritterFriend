d5e4e87cf2360d78af7092f0ebf1e193b8881b5d
aparant777 <aparant777mane@gmail.com>
2016-11-12 00:31:54 -0700

    added pathfinding, basic UI and set Project settings

---
+++ CritterFriend/Assets/Scripts/AI/Poacher.cs
@@ -0,0 +1,32 @@
+﻿/*this script is attached to the poacher gameobject*/
+using UnityEngine;
+using System.Collections;
+
+public class Poacher : MonoBehaviour {
+
+    public Transform target;   //the poacher will follow this target
+    public NavMeshAgent navMeshAgent;
+    public UIManager uiManager;
+    
+    
+   void Start() {
+        if(navMeshAgent == null) {
+            navMeshAgent = gameObject.GetComponent<NavMeshAgent>();
+        }
+
+        if(uiManager == null) {
+            uiManager = GameObject.FindGameObjectWithTag("UIManager").GetComponent<UIManager>();
+        }
+    } 
+
+    void Update() {
+        navMeshAgent.SetDestination(target.position);
+    }
+
+    void OnCollisionEnter(Collision other) {
+        if (other.gameObject.tag == "Target") {
+            Debug.Log("collided baby");
+            uiManager.EnableTextCollision();
+        }
+    }
+}
---
+++ CritterFriend/Assets/Scripts/Managers/UIManager.cs
@@ -0,0 +1,22 @@
+﻿/*this script is on UIManager*/
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+public class UIManager : MonoBehaviour {
+
+    public Text text_Poacher_Target_collision;
+
+    void Start() {
+        if(text_Poacher_Target_collision == null) {
+            Debug.Log("Text collision empty");
+        }else {
+            text_Poacher_Target_collision.enabled = false;
+        }
+        
+    }
+
+    /*is called by the Poacher class*/
+    public void EnableTextCollision() {
+        text_Poacher_Target_collision.enabled = true;
+    }
+}
