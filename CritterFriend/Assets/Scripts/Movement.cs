907978e2eae801a941c35767c95fd3bd4904b56c
Ajay Sathish <u1068959@ad.utah.edu>
2016-11-12 14:03:46 -0700

    Updated collision for streets

--- CritterFriend/Assets/Scripts/Movement.cs
+++ CritterFriend/Assets/Scripts/Movement.cs
@@ -5,23 +5,27 @@ public class Movement : MonoBehaviour {
     public float movementSpeed = 5.0f;
 
     public GameObject CameraEye;
+    public GameObject playerInitialPosition;
+    private Vector2 CameraEyeInitialState;
     public GameObject HMWRig;
 
   
-    private Transform CameraEyeInitialState;
+    //private Transform CameraEyeInitialState;
     // Use this for initialization
     void Start () {
-        CameraEyeInitialState = CameraEye.transform;
-        
-	}
+        CameraEyeInitialState = playerInitialPosition.transform.position;
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
         if (Input.GetKey(KeyCode.W) || HMWRig.GetComponent<Motion>().isAccelerationEnabled == true)
         {
             //print("hell");
-           // Debug.Log("Hell");
-            transform.position += new Vector3(CameraEye.transform.forward.x, CameraEye.transform.forward.y, CameraEye.transform.forward.z) * Time.deltaTime * movementSpeed;
+            // Debug.Log("Hell");
+            if (playerInitialPosition.transform.position.y + 0.3 >= CameraEyeInitialState.y)
+                transform.position += new Vector3(CameraEye.transform.forward.x, CameraEye.transform.forward.y, CameraEye.transform.forward.z) * Time.deltaTime * movementSpeed;
+           
         }
 
         if (Input.GetKey(KeyCode.S))
