9d9b0c6a0b5a9e3ef9bc953c0462cb0f6fc270c7
Ajay Sathish <u1068959@ad.utah.edu>
2016-11-12 11:16:41 -0700

    Movement script added

---
+++ CritterFriend/Assets/Scripts/Movement.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour {
+    public float movementSpeed = 5.0f;
+
+    public GameObject CameraEye;
+
+    private Transform CameraEyeInitialState;
+    // Use this for initialization
+    void Start () {
+        CameraEyeInitialState = CameraEye.transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.W))
+        {
+            //print("hell");
+           // Debug.Log("Hell");
+            transform.position += new Vector3(CameraEye.transform.forward.x, CameraEye.transform.forward.y, CameraEye.transform.forward.z) * Time.deltaTime * movementSpeed;
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position -= transform.forward * Time.deltaTime * movementSpeed;
+        }
+   }
+}
